2badbacf5bb472d10e9e5a2fd8dc7d20627961f8
Eric Weinerman <eric.weinerman@prolifiq.com>
2016-06-30 09:36:39 -0700

    small change to text on Main.php page

--- main.php
+++ main.php
@@ -4,13 +4,13 @@
 	<div id="HomePage_MainContainer">
 		<img height="125" width="125" class="TextWrapLeft" src="images/sc_logo_sm.png">
 		<p>
-			This website is designed to give VVARMachine an ability to manage our Star Citizen Division by recording number of players, ships and organizational structure.  If you are interested in joining VVARMachine please go to <a href="http://vvarmachine.com" target="_top">http://vvarmachine.com</a> or visit our RSI Organization page at <a href="https://robertsspaceindustries.com/orgs/WARMACHINE" target="_blank">https://robertsspaceindustries.com/orgs/WARMACHINE</a>.
+			This website is designed to give VVARMachine an ability to manage our Star Citizen Division by recording number of players, ships and organizational structure.  If you are interested in joining VVARMachine please go to <a href="http://vvarmachine.com" target="_top">http://vvarmachine.com</a> or visit our RSI Organization page at <a href="https://robertsspaceindustries.com/orgs/WARMACHINE" target="_blank">https://robertsspaceindustries.com/orgs/WARMACHINE</a>
 		</p>
 		<p>
-			You can also visit our Star Citizen partners (AMP) at <a href="http://ampalliance.net" target="_blank">http://ampalliance.net</a> or their RSI Organization page at <a href="https://robertsspaceindustries.com/orgs/AMP" target="_blank">https://robertsspaceindustries.com/orgs/AMP</a>.
+			You can also visit our Star Citizen partners (AMP) at <a href="http://ampalliance.net" target="_blank">http://ampalliance.net</a> or their RSI Organization page at <a href="https://robertsspaceindustries.com/orgs/AMP" target="_blank">https://robertsspaceindustries.com/orgs/AMP</a>
 		</p>
 		<p>
-			Use the menu options above to navigate this site to find out information about our Star Citizen Division.  Only ech0whiskey and Razzun currently have the ability to update the information on this site.  Please use the main website to contact one of them.
+			Use the menu options above to navigate this site to find out information about our Star Citizen Division. Only ech0whiskey and Razzun currently have the ability to update the information on this site. Please use the main website to contact one of them.
 		</p>
 		<p>
 			If you have technical issues please email us at webmaster [at] vvarmachine.com.
