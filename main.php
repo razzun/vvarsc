849b137beca283850201e97d4ceb09da42d1eb19
Russell Teicheira <Russell Teicheira>
2016-07-31 10:44:52 -0700

    Fixed logo on main page to display correctly when an error occures.

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 <div id="TEXT">
 
 	<div id="HomePage_MainContainer">
-		<img height="125" width="125" class="TextWrapLeft" src="images/sc_logo_sm.png">
+		<img height="125" width="125" class="TextWrapLeft" src="http://sc.vvarmachine.com/images/sc_logo_sm.png">
 		<p>
 			This website is designed to give VVARMachine an ability to manage our Star Citizen Division by recording number of players, ships and organizational structure.  If you are interested in joining VVARMachine please go to <a href="http://vvarmachine.com" target="_top">http://vvarmachine.com</a> or visit our RSI Organization page at <a href="https://robertsspaceindustries.com/orgs/WARMACHINE" target="_blank">https://robertsspaceindustries.com/orgs/WARMACHINE</a>
 		</p>
