44020eed9a709668c1d6ee1f85fb28cd8626b2e9
Russell Teicheira <Russell Teicheira>
2016-07-31 10:31:39 -0700

    Updated content on admin page

--- admin/admin.php
+++ admin/admin.php
@@ -1,8 +1,11 @@
 <h2>VVARMachine SCD - ADMIN ZONE</h2>
 <div id="TEXT">
 	<? include_once('admin_menu.php'); ?>
-	<h3>Home</h3>
-	<p>This is the future home of the website's administrative section.  All links to the left will be restricted to only those that have proper login credentials.  All links are currently broken as the pages and authentication function is still being developed.  Check back soon.</p>
-	<p>Thanks, Razzun<br /><br />
-	<? print_r ($_SERVER['DOCUMENT_ROOT']); ?></p>
+	<h3>Admin Zone Home</h3>
+	<p>Welcome back <? print_r ($_SESSION['sess_username']); ?>.  What would you like to do?</p>
+	<p><strong>Manufacturer Management - </strong> Used to manage the different manufacturer's used in Star Citizen.</br></br>
+    <strong>Member Management - </strong> Used to manage VVARMachine members.</br></br>
+    <strong>Ship Management - </strong> Not currently active.  Check back later.</br></br>
+    <strong>Roles Management - </strong> Used to manage the different roles within the VVARMachine Star Citizen Division.</br></br>
+    <strong>Unit Management - </strong> Used to manage the different unit groups within the VVARMachine Star Citizen Division.</p>
 </div>
