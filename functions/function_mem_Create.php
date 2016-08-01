849f05a5bca9077c3aa67c892391076a5d487bb8
Eric Weinerman <eric.weinerman@prolifiq.com>
2016-07-31 21:25:14 -0700

    new members will be created with hashed password

--- functions/function_mem_Create.php
+++ functions/function_mem_Create.php
@@ -60,7 +60,7 @@
 				,null
 				,DATE_ADD(CURDATE(),INTERVAL 930 YEAR)
 				,'user'
-				,'password'
+				,'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86'
 			)";
 
 	$query_result = $connection->query($q);
