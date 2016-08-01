06c7b71e0b85c39536400f5a244d4fa701c96aab
razzun <razzun@vvarnc.com>
2016-08-01 15:58:41 -0700

    Enabling "enter" on loging.

--- login.php
+++ login.php
@@ -41,12 +41,13 @@
 											   ?>  
 
 											<form action="authenticate.php" method="POST" class="form-signin col-md-8 col-md-offset-2" role="form">
-												<input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
+												<input type="text" name="username" class="form-control" placeholder="Username" onkeypress="entsub(this.form, this.form.password);" required autofocus>
 												<br/>
-												<input id="password" type="password" name="password" class="form-control" placeholder="Password" required>
+												<input id="password" type="password" name="password" class="form-control" placeholder="Password" onkeypress="entsub(this.form, this.form.password);" required>
 												<br/>
 												<input type="button" 
-														value="Login" 
+														id="button"
+														value="Login"
 														onclick="formhash(this.form, this.form.password);" /> 
 											</form>
 										</div>
@@ -69,132 +70,141 @@
 
 <!--Script to make parallax backgrounds full-height-->
 <script>
-/* detect touch */
-if("ontouchstart" in window){
-    document.documentElement.className = document.documentElement.className + " touch";
-}
-if(!$("html").hasClass("touch")){
-    /* background fix */
-    $(".parallax").css("background-attachment", "fixed");
-}
-
-/* fix vertical when not overflow call fullscreenFix() if .fullscreen content changes */
-function fullscreenFix(){
-    var h = $('body').height();
-	var h2 = $(".content-b").innerHeight();
-	
-    /* set .fullscreen height */
-	if(h2 > h)
-	{
-		$(".fullscreen").addClass('overflow');
+	/* detect touch */
+	if("ontouchstart" in window){
+		document.documentElement.className = document.documentElement.className + " touch";
 	}
-	
-	/*
-    $('.content-b').each(function(i){
-        if($(this).innerHeight() > h)
-		{
-			$(this).closest('.fullscreen').addClass("overflow");
-        }
-    });
-	*/
-}
-$(window).resize(fullscreenFix);
-fullscreenFix();
+	if(!$("html").hasClass("touch")){
+		/* background fix */
+		$(".parallax").css("background-attachment", "fixed");
+	}
 
-/* resize background images */
-function backgroundResize(){
-    var windowH = $(window).height();
-    $(".background").each(function(i){
-        var path = $(this);
-        /* variables */
-        var contW = path.width();
-        var contH = path.height();
-        var imgW = path.attr("data-img-width");
-        var imgH = path.attr("data-img-height");
-        var ratio = imgW / imgH;
-        /* overflowing difference */
-        var diff = parseFloat(path.attr("data-diff"));
-        diff = diff ? diff : 0;
-        /* remaining height to have fullscreen image only on parallax */
-        var remainingH = 0;
-        if(path.hasClass("parallax") && !$("html").hasClass("touch")){
-            var maxH = contH > windowH ? contH : windowH;
-            remainingH = windowH - contH;
-        }
-        /* set img values depending on cont */
-        imgH = contH + remainingH + diff;
-        imgW = imgH * ratio;
-        /* fix when too large */
-        if(contW > imgW){
-            imgW = contW;
-            imgH = imgW / ratio;
-        }
+	/* fix vertical when not overflow call fullscreenFix() if .fullscreen content changes */
+	function fullscreenFix(){
+		var h = $('body').height();
+		var h2 = $(".content-b").innerHeight();
 		
-        path.data("resized-imgW", imgW);
-        path.data("resized-imgH", imgH);
-        path.css("background-size", imgW + "px " + imgH + "px");
-    });
-}
-$(window).resize(backgroundResize);
-$(window).focus(backgroundResize);
-backgroundResize();
+		/* set .fullscreen height */
+		if(h2 > h)
+		{
+			$(".fullscreen").addClass('overflow');
+		}
+		
+		/*
+		$('.content-b').each(function(i){
+			if($(this).innerHeight() > h)
+			{
+				$(this).closest('.fullscreen').addClass("overflow");
+			}
+		});
+		*/
+	}
+	$(window).resize(fullscreenFix);
+	fullscreenFix();
 
-/* set parallax background-position */
-function parallaxPosition(e){
-    var heightWindow = $(window).height();
-    var topWindow = $(window).scrollTop();
-    var bottomWindow = topWindow + heightWindow;
-    var currentWindow = (topWindow + bottomWindow) / 2;
-    $(".parallax").each(function(i){
-        var path = $(this);
-        var height = path.height();
-        var top = path.offset().top;
-        var bottom = top + height;
-        /* only when in range */
-        if(bottomWindow > top && topWindow < bottom){
-            var imgW = path.data("resized-imgW");
-            var imgH = path.data("resized-imgH");
-            /* min when image touch top of window */
-            var min = 0;
-            /* max when image touch bottom of window */
-            var max = - imgH + heightWindow;
-            /* overflow changes parallax */
-            var overflowH = height < heightWindow ? imgH - height : imgH - heightWindow;
-			/* fix height on overflow */
-            top = top - overflowH;
-            bottom = bottom + overflowH;
-            /* value with linear interpolation */
-            var value = min + (max - min) * (currentWindow - top) / (bottom - top);
-            /* set background-position */
-            var orizontalPosition = path.attr("data-oriz-pos");
-            orizontalPosition = orizontalPosition ? orizontalPosition : "50%";
-            $(this).css("background-position", orizontalPosition + " " + value + "px");
-        }
-    });
-}
-if(!$("html").hasClass("touch")){
-    $(window).resize(parallaxPosition);
-    $(window).scroll(parallaxPosition);
-    parallaxPosition();
-}
+	/* resize background images */
+	function backgroundResize(){
+		var windowH = $(window).height();
+		$(".background").each(function(i){
+			var path = $(this);
+			/* variables */
+			var contW = path.width();
+			var contH = path.height();
+			var imgW = path.attr("data-img-width");
+			var imgH = path.attr("data-img-height");
+			var ratio = imgW / imgH;
+			/* overflowing difference */
+			var diff = parseFloat(path.attr("data-diff"));
+			diff = diff ? diff : 0;
+			/* remaining height to have fullscreen image only on parallax */
+			var remainingH = 0;
+			if(path.hasClass("parallax") && !$("html").hasClass("touch")){
+				var maxH = contH > windowH ? contH : windowH;
+				remainingH = windowH - contH;
+			}
+			/* set img values depending on cont */
+			imgH = contH + remainingH + diff;
+			imgW = imgH * ratio;
+			/* fix when too large */
+			if(contW > imgW){
+				imgW = contW;
+				imgH = imgW / ratio;
+			}
+			
+			path.data("resized-imgW", imgW);
+			path.data("resized-imgH", imgH);
+			path.css("background-size", imgW + "px " + imgH + "px");
+		});
+	}
+	$(window).resize(backgroundResize);
+	$(window).focus(backgroundResize);
+	backgroundResize();
+
+	/* set parallax background-position */
+	function parallaxPosition(e){
+		var heightWindow = $(window).height();
+		var topWindow = $(window).scrollTop();
+		var bottomWindow = topWindow + heightWindow;
+		var currentWindow = (topWindow + bottomWindow) / 2;
+		$(".parallax").each(function(i){
+			var path = $(this);
+			var height = path.height();
+			var top = path.offset().top;
+			var bottom = top + height;
+			/* only when in range */
+			if(bottomWindow > top && topWindow < bottom){
+				var imgW = path.data("resized-imgW");
+				var imgH = path.data("resized-imgH");
+				/* min when image touch top of window */
+				var min = 0;
+				/* max when image touch bottom of window */
+				var max = - imgH + heightWindow;
+				/* overflow changes parallax */
+				var overflowH = height < heightWindow ? imgH - height : imgH - heightWindow;
+				/* fix height on overflow */
+				top = top - overflowH;
+				bottom = bottom + overflowH;
+				/* value with linear interpolation */
+				var value = min + (max - min) * (currentWindow - top) / (bottom - top);
+				/* set background-position */
+				var orizontalPosition = path.attr("data-oriz-pos");
+				orizontalPosition = orizontalPosition ? orizontalPosition : "50%";
+				$(this).css("background-position", orizontalPosition + " " + value + "px");
+			}
+		});
+	}
+	if(!$("html").hasClass("touch")){
+		$(window).resize(parallaxPosition);
+		$(window).scroll(parallaxPosition);
+		parallaxPosition();
+	}
 </script>
 
 <script>
-
-function formhash(form, password){
-    // Create a new element input, this will be our hashed password field. 
-    var p = document.createElement("input");
- 
-    // Add the new element to our form. 
-    form.appendChild(p);
-    p.name = "p";
-    p.type = "hidden";
-    p.value = hex_sha512(password.value);
- 
-    // Make sure the plaintext password doesn't get sent. 
-    password.value = "";
- 
-    // Finally submit the form. 
-    form.submit();
-}
+	function formhash(form, password){
+		// Create a new element input, this will be our hashed password field. 
+		var p = document.createElement("input");
+	 
+		// Add the new element to our form. 
+		form.appendChild(p);
+		p.name = "p";
+		p.type = "hidden";
+		p.value = hex_sha512(password.value);
+	 
+		// Make sure the plaintext password doesn't get sent. 
+		password.value = "";
+	 
+		// Finally submit the form. 
+		form.submit();
+	}
+	
+	function entsub(form, password) {
+	  if ((window.event && window.event.keyCode == 13) || (which.event && which.event.keyCode == 13)) {
+		formhash(form, password);
+	  }
+	  else {
+		//password.value = "";
+		return true;
+	  }
+	}
 </script>
