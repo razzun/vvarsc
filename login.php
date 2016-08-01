e3a0c8363e1e4213304ce47c388e39b64ea9d399
Eric Weinerman <eric.weinerman@prolifiq.com>
2016-07-31 18:11:52 -0700

    reverted previous changed to login page

--- login.php
+++ login.php
@@ -1,97 +1,30 @@
-<html>
-	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<? include_once('inc/meta.php'); ?>
-	</head>
-<body>
-	<div class="fullscreen background parallax" data-img-width="3840" data-img-height="2160" data-diff="100">
-		<div class="content-a" id="parallax__layer--base">
-			<div class="content-b">
-				<div id="MainWrapper">
-					<div id="MainWebsiteInner">
-						<? include_once('inc/header.php'); ?>
-						<div id="CONTENT">
-							<h2>VVARMachine Star Citizen Division Home</h2>
-							<div id="TEXT">
-								<!--Login Section-->
-								<div class="col-md-6 col-md-offset-3">
-									<h4>
-										Log in with your credentials
-									</h4>
-									<br/>
-									<div class="block-margin-top">
-										  <?php 
-
-											$errors = array(
-												1=>"Invalid user name or password, Try again",
-												2=>"Please login to access this area",
-												3=>"Unexpected Server Error Occurred with Database Connection - please inform website Admin!",
-												4=>"This Area Requires Admin Access - we're really sorry, but you're not an Admin!"
-											  );
-
-											$error_id = isset($_GET['err']) ? (int)$_GET['err'] : 0;
-
-											if ($error_id != 0) {
-													echo '<p class="text-danger">'.$errors[$error_id].'</p>';
-												}
-										   ?>  
-
-										<form action="authenticate.php" method="POST" class="form-signin col-md-8 col-md-offset-2" role="form">
-											<input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
-											<br/>
-											<input type="password" name="password" class="form-control" placeholder="Password" required>
-											<br/>
-											<button class="btn btn-lg btn-primary btn-block" type="submit">
-												Sign in
-											</button>
-										</form>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<? include_once('inc/footer.php'); ?>
-			</div>
-		</div>
-	</div>
-</body>
-
-
+<html>	<head>		<meta name="viewport" content="width=device-width, initial-scale=1.0">		<? include_once('inc/meta.php'); ?>	</head><body>	<div class="fullscreen background parallax" data-img-width="3840" data-img-height="2160" data-diff="100">		<div class="content-a" id="parallax__layer--base">			<div class="content-b">				<div id="MainWrapper">					<div id="MainWebsiteInner">						<? include_once('inc/header.php'); ?>						<div id="CONTENT">							<h2>VVARMachine Star Citizen Division Home</h2>							<div id="TEXT">								<!--Login Section-->								<div class="col-md-6 col-md-offset-3">									<h4>										Log in with your credentials									</h4>									<br/>									<div class="block-margin-top">										  <?php 											$errors = array(												1=>"Invalid user name or password, Try again",												2=>"Please login to access this area",												3=>"Unexpected Server Error Occurred with Database Connection - please inform website Admin!",												4=>"This Area Requires Admin Access - we're really sorry, but you're not an Admin!"											  );											$error_id = isset($_GET['err']) ? (int)$_GET['err'] : 0;											if ($error_id != 0) {													echo '<p class="text-danger">'.$errors[$error_id].'</p>';												}										   ?>  										<form action="authenticate.php" method="POST" class="form-signin col-md-8 col-md-offset-2" role="form">											<input type="text" name="username" class="form-control" placeholder="Username" required autofocus>											<br/>											<input type="password" name="password" class="form-control" placeholder="Password" required>											<br/>											<button class="btn btn-lg btn-primary btn-block" type="submit">												Sign in											</button>										</form>									</div>								</div>							</div>						</div>					</div>				</div>				<? include_once('inc/footer.php'); ?>			</div>		</div>	</div></body>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script type="text/javascript" src="/js/jquery.jScale.js"></script>
-<script type="text/javascript" src="http://www.wduffy.co.uk/blog/wp-content/themes/agregado/js/jquery.jscroll.min.js"></script>
-
+<script type="text/javascript" src="http://www.wduffy.co.uk/blog/wp-content/themes/agregado/js/jquery.jscroll.min.js"></script>
 <!--Script to Re-Size Page to 100% Height based on viewport size -->
 <script>
 	$(document).ready(function() {
-	
 		var height = $(window).height();
 		var websiteClass = $('#CONTENT');
 		var spacerClass = $('#dynamicSpacer');
 		
 		var footer = $('#FOOTER');
-		var fHeight = footer.height();
-		
+		var fHeight = footer.height();
 		var minHeight = (height - websiteClass.offset().top - 8);
-		var spacerHeight = (minHeight - spacerClass.offset().top - fHeight + 20);
-		
+		var spacerHeight = (minHeight - spacerClass.offset().top - fHeight + 20);		
 		//Set Element Properties
 		websiteClass.css({
-			"min-height": minHeight +'px'
-		});
-		
+			"min-height": minHeight +'px'		});		
 		spacerClass.css({
 			"height": spacerHeight +'px'
 		});
 	});
 	
-	$(window).resize(function () {
-	
+	$(window).resize(function () {	
 		var height = $(window).height();
 		var websiteClass = $('#CONTENT');
-		var spacerClass = $('#dynamicSpacer');
-		
+		var spacerClass = $('#dynamicSpacer');	
 		var footer = $('#FOOTER');
 		var fHeight = footer.height();
 		
@@ -101,11 +34,9 @@
 		//Set Element Properties
 		websiteClass.css({
 			"min-height": minHeight +'px'
-		});
-		
+		});		
 		spacerClass.css({
 			"height": spacerHeight +'px'
 		});		
 	});	
-
 </script>
