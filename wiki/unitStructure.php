1e85025d684dc89fdb66277823f758371b2cd569
Eric Weinerman <eric.weinerman@prolifiq.com>
2016-09-11 09:01:53 -0700

    wiki changes

--- wiki/unitStructure.php
+++ wiki/unitStructure.php
@@ -1,189 +1,10 @@
-<?php include_once('functions/function_auth_user.php'); ?>
-<?
-	$enlistedRanksQuery = "
-		select distinct
-			r.rank_level
-			,(select rank_abbr from projectx_vvarsc2.ranks r2 where r2.rank_level = r.rank_level and r2.rank_type = 'Navy') as 'rank_abbr_navy'
-			,(select rank_name from projectx_vvarsc2.ranks r2 where r2.rank_level = r.rank_level and r2.rank_type = 'Navy') as 'rank_name_navy'
-			,(select rank_tinyImage from projectx_vvarsc2.ranks r2 where r2.rank_level = r.rank_level and r2.rank_type = 'Navy') as 'rank_tinyImage_navy'
-			,(select rank_abbr from projectx_vvarsc2.ranks r2 where r2.rank_level = r.rank_level and r2.rank_type = 'USMC') as 'rank_abbr_usmc'
-			,(select rank_name from projectx_vvarsc2.ranks r2 where r2.rank_level = r.rank_level and r2.rank_type = 'USMC') as 'rank_name_usmc'
-			,(select rank_tinyImage from projectx_vvarsc2.ranks r2 where r2.rank_level = r.rank_level and r2.rank_type = 'USMC') as 'rank_tinyImage_usmc'
-			,(select distinct rank_groupName from projectx_vvarsc2.ranks r2 where r2.rank_level = r.rank_level) as 'rank_groupName'
-		from projectx_vvarsc2.ranks r
-		where r.rank_level like 'E-%'
-		order by
-			r.rank_orderby desc	
-	";
-	
-	$enlistedTable = "
-		<table class=\"adminTable\" style=\"width: auto; display: table-row\">
-			<tr class=\"adminTableHeaderRow\">
-				<th class=\"adminTableHeaderRowTD\">
-					PayGrade
-				</th>
-				<th class=\"adminTableHeaderRowTD\">
-					Navy Rank
-				</th>
-				<th class=\"adminTableHeaderRowTD\">
-					Marine Rank
-				</th>
-				<th class=\"adminTableHeaderRowTD\">
-					RSI Organization Rank
-				</th>
-			</tr>	
-	";	
-
-	$enlistedResult = $connection->query($enlistedRanksQuery);
-	while(($row1 = $enlistedResult->fetch_assoc()) != false) {
-		$rank_level = $row1['rank_level'];
-		$rank_abbr_navy = $row1['rank_abbr_navy'];
-		$rank_name_navy = $row1['rank_name_navy'];
-		$rank_tinyImage_navy = $row1['rank_tinyImage_navy'];
-		$rank_abbr_usmc = $row1['rank_abbr_usmc'];
-		$rank_name_usmc = $row1['rank_name_usmc'];
-		$rank_tinyImage_usmc = $row1['rank_tinyImage_usmc'];
-		$rank_groupName = $row1['rank_groupName'];
-		
-		$rankNameNavy = "- not used -";
-		if ($rank_abbr_navy != null && $rank_name_navy != null)
-		{
-			$rankNameNavy = "$rank_abbr_navy - $rank_name_navy";
-		}
-		$rankNameUSMC = "- not used -";
-		if ($rank_abbr_usmc != null && $rank_name_usmc != null)
-		{
-			$rankNameUSMC = "$rank_abbr_usmc - $rank_name_usmc";
-		}		
-		
-		$enlistedTable .= "
-			<tr class=\"adminTableRow\">
-				<td class=\"adminTableRowTD\">
-					$rank_level
-				</td>
-				<td class=\"adminTableRowTD\">
-					<div class=\"clickableRow_memRank_inner\">
-						<img class=\"clickableRow_memRank_Image\" src=\"http://sc.vvarmachine.com/images/ranks/TS3/$rank_tinyImage_navy.png\" />
-						<div class=\"rank_image_text\">
-							$rankNameNavy
-						</div>
-					</div>
-				</td>
-				<td class=\"adminTableRowTD\" style=\"text-align: left\">
-					<div class=\"clickableRow_memRank_inner\">
-						<img class=\"clickableRow_memRank_Image\" src=\"http://sc.vvarmachine.com/images/ranks/TS3/$rank_tinyImage_usmc.png\" />
-						<div class=\"rank_image_text\">
-							$rankNameUSMC
-						</div>
-					</div>
-				</td>
-				<td class=\"adminTableRowTD\" style=\"text-align: left\">
-					$rank_groupName
-				</td>						
-			</tr>			
-		";
-	}
-	
-	$enlistedTable .= "
-		</table>	
-	";
-	
-	$officerRanksQuery = "
-		select distinct
-			r.rank_level
-			,(select rank_abbr from projectx_vvarsc2.ranks r2 where r2.rank_level = r.rank_level and r2.rank_type = 'Navy') as 'rank_abbr_navy'
-			,(select rank_name from projectx_vvarsc2.ranks r2 where r2.rank_level = r.rank_level and r2.rank_type = 'Navy') as 'rank_name_navy'
-			,(select rank_tinyImage from projectx_vvarsc2.ranks r2 where r2.rank_level = r.rank_level and r2.rank_type = 'Navy') as 'rank_tinyImage_navy'
-			,(select rank_abbr from projectx_vvarsc2.ranks r2 where r2.rank_level = r.rank_level and r2.rank_type = 'Common') as 'rank_abbr_common'
-			,(select rank_name from projectx_vvarsc2.ranks r2 where r2.rank_level = r.rank_level and r2.rank_type = 'Common') as 'rank_name_common'
-			,(select rank_tinyImage from projectx_vvarsc2.ranks r2 where r2.rank_level = r.rank_level and r2.rank_type = 'Common') as 'rank_tinyImage_common'
-			,(select distinct rank_groupName from projectx_vvarsc2.ranks r2 where r2.rank_level = r.rank_level) as 'rank_groupName'
-		from projectx_vvarsc2.ranks r
-		where r.rank_level like 'O-%'
-		order by
-			r.rank_orderby desc	
-	";
-	
-	$officerTable = "
-		<table class=\"adminTable\" style=\"width: auto; display: table-row\">
-			<tr class=\"adminTableHeaderRow\">
-				<th class=\"adminTableHeaderRowTD\">
-					PayGrade
-				</th>
-				<th class=\"adminTableHeaderRowTD\">
-					Navy Rank
-				</th>
-				<th class=\"adminTableHeaderRowTD\">
-					Marine Rank
-				</th>
-				<th class=\"adminTableHeaderRowTD\">
-					RSI Organization Rank
-				</th>
-			</tr>	
-	";	
-
-	$officerResult = $connection->query($officerRanksQuery);
-	while(($row1 = $officerResult->fetch_assoc()) != false) {
-		$rank_level = $row1['rank_level'];
-		$rank_abbr_navy = $row1['rank_abbr_navy'];
-		$rank_name_navy = $row1['rank_name_navy'];
-		$rank_tinyImage_navy = $row1['rank_tinyImage_navy'];
-		$rank_abbr_common = $row1['rank_abbr_common'];
-		$rank_name_common = $row1['rank_name_common'];
-		$rank_tinyImage_common = $row1['rank_tinyImage_common'];
-		$rank_groupName = $row1['rank_groupName'];
-		
-		$rankNameNavy = "- not used- ";
-		if ($rank_abbr_navy != null && $rank_name_navy != null)
-		{
-			$rankNameNavy = "$rank_abbr_navy - $rank_name_navy";
-		}
-		$rankNameCommon = "- not used -";
-		if ($rank_abbr_common != null && $rank_name_common!= null)
-		{
-			$rankNameCommon = "$rank_abbr_common - $rank_name_common";
-		}			
-		
-		if ($rank_tinyImage_common == null)
-			$rank_tinyImage_common = $rank_tinyImage_navy;
-		
-		$officerTable .= "
-			<tr class=\"adminTableRow\">
-				<td class=\"adminTableRowTD\">
-					$rank_level
-				</td>
-				<td class=\"adminTableRowTD\">
-					<div class=\"clickableRow_memRank_inner\">
-						<img class=\"clickableRow_memRank_Image\" src=\"http://sc.vvarmachine.com/images/ranks/TS3/$rank_tinyImage_navy.png\" />
-						<div class=\"rank_image_text\">
-							$rankNameNavy
-						</div>
-					</div>
-				</td>
-				<td class=\"adminTableRowTD\" style=\"text-align: left\">
-					<div class=\"clickableRow_memRank_inner\">
-						<img class=\"clickableRow_memRank_Image\" src=\"http://sc.vvarmachine.com/images/ranks/TS3/$rank_tinyImage_common.png\" />
-						<div class=\"rank_image_text\">
-							$rankNameCommon
-						</div>
-					</div>
-				</td>
-				<td class=\"adminTableRowTD\" style=\"text-align: left\">
-					$rank_groupName
-				</td>						
-			</tr>			
-		";
-	}
-	
-	$officerTable .= "
-		</table>	
-	";	
-	
-	$connection->close();
-
-?>
-
-<h2>Fleet Rank Structure</h2>
+<br />
+<div class="div_filters_container">
+	<div class="div_filters_entry">
+		<a href="http://sc.vvarmachine.com/wiki/">&#8672; Back to Wiki Home</a>
+	</div>
+</div>
+<h2>Fleet Unit Organizational Structure</h2>
 <div id="TEXT">
 
 	<div id="rankStructure_main">
@@ -191,7 +12,7 @@
 		</div>
 		<div class="unit_details_container">
 			<p style="text-align: center; font-style: italic">
-				The VVarMachine Combined Fleet uses a Hierarchical Military Unit Structure and Ranking System, with design elements incorporated from the organization of the US Navy, AirForce, and Marines.
+				The VVarMachine Combined Fleet uses a Hierarchical Military Unit Structure, with design elements incorporated from the organization of the US Navy, AirForce, and Marine Corps. In broad terms, our fleet is arranged into two branches <strong>(Divisions)</strong>. Our Military Division will focus on combat-related duties, and our Economic Division will focus on generating revenue to support the war-effort.
 			</p>
 			<div class="shipDetails_info1_table_ship_desc" style="font-style: normal">
 				<div class="corner corner-top-left">
@@ -202,36 +23,44 @@
 				</div>
 				<div class="corner corner-bottom-right">
 				</div>
-				<h3 style="padding-left: 4px; margin-left: 0">Fleet Ranks and PayGrades</h3>
-				In armed-forces around the world, a member's rank is defined by a <strong>PayGrade</strong>, and a <strong>Role</strong> that the member performs. As the member becomes more proficient in the skills needed to perform that Role, he or she can receive promotion to a higher PayGrade. When the member is sufficiently qualified in their speciality, and has reached the appropriate PayGrade, they can be chosen to lead and develop the skills of lower-grade members in their field. PayGrades are divided into three groups:
-				<br />
-				<ul>
-					<li style="color: #FFF; font-style: italic">
-						<a href="#rankDetails_enlisted">Enlisted Ranks</a>
-					</li>
-					<li style="color: #FFF; font-style: italic">Warrant-Officer Ranks (not used in the VVarMachine Fleet)</li>
-					<li style="color: #FFF; font-style: italic">
-						<a href="#rankDetails_officer">Officer Ranks</a>
-					</li>
-				</ul>
-				Because there are far more PayGrades than there are supported ranks in the RSI Organization Structure, each group of PayGrades can be divided further into sub-categories. These sub-categories relate directly to the six rank-levels of the RSI Organization Structure.
-				<h4 style="padding-left: 8px; margin-left: 0; font-size: 12pt">Enlisted Ranks</h4>
-				<? echo $enlistedTable ?>
-				<h4 style="padding-left: 8px; margin-left: 0; font-size: 12pt">Officer Ranks</h4>
-				<? echo $officerTable ?>
-				<br />
-				Military Flight Units and Division/Fleet Command utilize the Navy Rank Names and Images. Marine Names and Images are used by dedicated ground combat units, along with Quick Reaction Force (QRF) Units.
+				<h3 style="padding-left: 4px; margin-left: 0">Military Division Organization</h3>
+				<h4 style="padding-left: 8px; margin-left: 0; font-size: 12pt">Unit Types</h4>
+				Our Military Division is built using a unit-hierarchy system. The lowest-level of the tree, and smallest permanent units are <strong>Squadrons</strong> and <strong>Platoons</strong>. These units are commanded by Junior Officers (O-1, O-2, O-3).
 				<br />
 				<br />
-				Details about each PayGrade can be found below, including the requirements for advancement from one to the next.
+				Each Squadron or Platoon will sortie one or more <strong>Flights</strong> or <strong>Teams</strong> for organized operations. These Flights/Teams are led by NCOs, but are not considered Permanent Units within the Fleet. For air units, a Flight consists of at least 2 ships, and the number of ships per Flight will vary based on the type of equipment operated by each Squadron during a particular operation. For ground units, a Team consists of no-more than 6 members.
+				<br />
+				<br />
+				Once a Squadron or Platoon has reached its Player Capacity (not yet defined), a sister-unit can be formed alongside it. This will generally be accomplished by offering an Officer Commission to the Assistant Squadron/Platoon Leader of the existing unit, and promoting another NCO (likely a Flight/Team Leader into the role of Assistant for the existing Unit). The new leader will be promoted to O-1, and be placed in-command of the newly-created unit.
+				<br />
+				<br />
+				Moving up the hierarchy tree, we find the <strong>Air Wing</strong> and <strong>Company</strong>-level units. These units will only be established once there are at-minimum 3 Squadrons/Platoons existing below them. The Air Wing and Company units will be commanded by an Officer of at-least O-3 PayGrade, along with a Senior NCO serving as their adviser.
+				<br />
+				<br />
+				The next level in the tree holds the <strong>Combat Group</strong> and <strong>Battalion</strong>-level units. These units are commanded by a Senior Officer (at least O-5 PayGrade), along with an O-4 Officer serving as the unit's Executive Officer (XO) and second-in-command. In addition, one or more Senior NCO members may be assigned to the unit to serve as an adviser, or perform highly-specilized roles outside of the normal duties of the units below them. As is the case with Air Wing and Company-level units, the formation of a Combat Group or Battalion will only take place when a minimum of 3 Air-Wings or Companies exist below them, and there is a need for a parent-unit to be created for oversight in planning and operational execution.
+				<br />
+				<br />
+				Finally, we reach the level of the Division Headquarters (DIV HQ). While units at the level of Air Wing/Company, or Combat Group/Battalion will not exist until needed, the DIV HQ is already established with leadership assigned. This means that any lower-level units will report directly to DIV HQ until the Fleet grows enough to create their immediate parent units. Each Division HQ is commanded by a Flag Officer (Admiral) of at-least O-7 PayGrade, who is assisted by a Senior Officer of at-least O-5 PayGrade. In addition, just like Combat Groups and Battalions, Senior NCOs will be present and assigned as advisers or specialized operatives performing unconventional roles for their Division or the Fleet. DIV HQ may also include other Officers working in non-operational roles such as Intelligence, Finance, Public Relations, Recruitment, etc.
+				<br />
+				<br />
+				<h4 style="padding-left: 8px; margin-left: 0; font-size: 12pt">Unit Numbering and Naming</h4>
+				Larger-size units such as Combat Groups/Battalions and Air Wings/Companies are numbered in the order they are created - meaning that the first Combat Group created within the Military Division will be numbered and named "1st Combat Group", same for the "1st Marine Battalion".
+				<br />
+				<br />
+				For Air Wings, these are also numbered by type - examples of this are "1st Fighter Wing", "1st Bomber Wing", etc. There will be no reuse of these names within the Fleet. The second Fighter Wing established will be called the "2nd Fighter Wing", regardless of which Combat Group it belongs to.
+				<br />
+				<br />
+				For Marine Companies, these are named by Phonetic letter in alphabetical order ("Alpha Company", "Bravo Company", etc), where names will be reused within each Battalion.
+				<br />
+				<br />
+				Squadrons are named with a three-digit number, indicative of the Combat Group and Air Wing to which they belong. For example, the "112th Fighter Squadron" belongs to the 1st Combat Group, and the first Air Wing created within that group, regardless of the Wing's type (in this case, the "1st Fighter Wing"). Due to this numbering system, the maximum amount of Squadrons within an Air Wing is limited to 9 for confusion to be avoided, and for proper use of the three-digit system. For Example, the "2nd Combat Group" may contain the "1st Fighter Wing", "4th Bomber Wing", and "7th Combat Support Wing", with each Wing's Squadrons being numbered 211st through 219th, 221st through 229th, and 231st through 239th, respectively - note how the number assigned to the Air Wing has no impact on the Squadron's number. That 2nd digit is derived from the order of the Air Wings when they are created within the Combat Group. Based on this system, there can only be one Squadron within the entire Fleet with a given three-digit number.
+				<br />
+				<br />
+				For Marine Platoons, these are numbered in-order of creation within a Company ("1st Platoon", "2nd Platoon", etc), where names will be reused within each Company.
+				<br />
+				<br />
 			</div>
 		</div>
-		<br />
-		<br />
-		<?include_once("wiki/ranks/rankDetails_enlisted.php");?>
-		<br />
-		<br />
-		<?include_once("wiki/ranks/rankDetails_officer.php");?>
 	</div>
 	
 </div>
