9372b50a04e7922bc145fb0e3651b9c6ec713534
Eric Weinerman <ech0whiskey.bf3@gmail.com>
2017-12-07 10:03:04 -0800

    test fix #2

--- mission.php
+++ mission.php
@@ -829,7 +829,7 @@
 					$display_opUnit_member_list
 				";				
 			}
-			else if ($opUnitsListItem_Support_AirFlight == "1")
+			if ($opUnitsListItem_Support_AirFlight == "1")
 			{
 				$display_opUnitShips_list_edit = "";
 				/*
