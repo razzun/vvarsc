9b4ddba395ad4d27347c48010af20cd0c90a9c1d
Razzun <razzun.vvargc+github@gmail.com>
2018-05-09 09:26:49 -0700

    Added a description for the new calendar page

--- calendar.php
+++ calendar.php
@@ -6,6 +6,7 @@
 <br />
 <div id="TEXT">
 	<div class="divinfo_container">
+		<p>All dates and times listed below are in UTC time.  Member can add this calendar to their own Google Calendar and view the times in their time zone.  Events can only be added by Senior Officers.  If you would like to plan an event and are not a Senior Officer, please contact the Senior Officer in your change-of-command.</p>
 		<center>
 			<iframe src="https://calendar.google.com/calendar/embed?mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=06pk4p2mk17t81u7ku4q0474c4%40group.calendar.google.com&amp;color=%230F4B38&amp;ctz=Etc%2FGMT" style="border-width:0" width="1200" height="600" frameborder="0" scrolling="no"></iframe>
 		</center>
