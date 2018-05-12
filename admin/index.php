2e92971b93fd1af5218d29feefd1e05766d819b7
Razzun <razzun.vvargc+github@gmail.com>
2018-05-12 15:48:29 -0700

    Added <meta name="robots" content="noindex,nofollow"> to /wiki/index.php and /admin/index.php

--- admin/index.php
+++ admin/index.php
@@ -8,6 +8,7 @@
 <html>
 	<head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="robots" content="noindex,nofollow">
 	<? include_once('../inc/meta.php'); ?>
 	</head>
 	<body>
