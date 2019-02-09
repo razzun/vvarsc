8e82a9d169f5f3f1adb768d68bab82c3bed72381
Razzun <razzun.vvargc+github@gmail.com>
2019-02-09 09:17:25 -0800

    Minor updates to Calendar (updated the link to the sheet and text on page), Main page (added link to RSI Org Page) and Footer (removed link to templates website, added email address for Webmaster).

--- inc/footer.php
+++ inc/footer.php
@@ -18,7 +18,7 @@
 		<div class="corner2 corner-bottom-right">
 		</div>
 		<p>
-			<a href="http://vvarmachine.com" target="_top">VVARMachine Home</a> &bull; <a href="http://coc.vvarmachine.com" target="_top">About Us &#38; CoC</a>
+			<a href="http://vvarmachine.com" target="_top">VVARMachine Home</a> &bull; <a href="http://coc.vvarmachine.com" target="_top">About Us &#38; CoC</a> &bull; <a href="mailto:webmaster@vvarmachine.com?subject=VVarMachine SC Site Question">Webmaster</a>
 		</p>
 		<p>
 			<a href="http://www.russteicheira.net" target="_blank">RussTeicheira.net</a> &copy; 2012-<?php echo date("Y"); ?>
@@ -27,9 +27,6 @@
 			Ship Manufacturer Logo Images &copy; 2012-<?php echo date("Y"); ?>
 			<br />
 			<a href="https://cloudimperiumgames.com/" target="_blank">Cloud Imperium Games Corporation</a> & <a href="https://robertsspaceindustries.com/" target="_blank">Roberts Space Industries Corp.</a>
-		</p>
-		<p>
-			<a href="http://www.gettemplate.com" target="_blank">Designed by GetTemplate.com</a>
 		</p>
 	</div>
 </div>
