b9bace3bcf17ecd4b6b0acc5db8beaadbc4deb73
Razzun <razzun.vvargc+github@gmail.com>
2019-03-23 09:29:25 -0700

    Sync

--- admin/admin_menu.php
+++ admin/admin_menu.php
@@ -1,9 +1,9 @@
-<div id="admin_menu">	<div class="corner corner-top-left">	</div>	<div class="corner corner-top-right">	</div>	<div class="corner corner-bottom-left">	</div>	<div class="corner corner-bottom-right">	</div>	
-    <ul>		<li><a href="?page=admin_awards">Awards</a></li>
-        <li><a href="?page=admin_manu">Manufacturers</a></li>
-        <li><a href="?page=admin_mem">Members</a></li>
-        <li><a href="#">Ships</a></li>		<li><a href="?page=admin_qual">Qualifications</a></li>
-        <li><a href="?page=admin_roles">Roles</a></li>
-        <li><a href="?page=admin_units">Units</a></li>
-    </ul>
+<div id="admin_menu">	<div class="corner corner-top-left">	</div>	<div class="corner corner-top-right">	</div>	<div class="corner corner-bottom-left">	</div>	<div class="corner corner-bottom-right">	</div>	
+    <ul>		<li><a href="?page=admin_awards">Awards</a></li>
+        <li><a href="?page=admin_manu">Manufacturers</a></li>
+        <li><a href="?page=admin_mem">Members</a></li>
+        <li><a href="#">Ships</a></li>		<li><a href="?page=admin_qual">Qualifications</a></li>
+        <li><a href="?page=admin_roles">Roles</a></li>
+        <li><a href="?page=admin_units">Units</a></li>
+    </ul>
 </div>
